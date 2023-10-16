c2b3c92b74b5cbd6d7c2d500ec61eb2dce05920a
polivovn <139216581+polivovn@users.noreply.github.com>
2023-10-16 19:24:09 +0500

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,16 @@
 package ru.netology.service;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
+    @org.tetsng.annotations.Test
+    public void testRemainWithBoundaryValue() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
 }
