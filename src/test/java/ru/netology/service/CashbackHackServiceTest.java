ee7a30dc1fba2a80d37a19bb9d0b823476e3184a
Polina <pooina123@gmail.com>
2023-10-16 19:02:52 +0500

    junit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,19 @@
 package ru.netology.service;
 
+
+import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemainWithBoundaryValue() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
 }
